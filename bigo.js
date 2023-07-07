3ed940582b7fd09043f37b0f576582ea2f8ff054
michael frederickson <mpfplus@gmail.com>
2023-07-07 13:03:29 -0600

    WIP: assigning Big O to various functions

---
+++ bigo.js
@@ -0,0 +1,69 @@
+"use strict";
+
+function logUpTo(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+// Time Complexity: O(n)
+
+function logAtLeast10(n) {
+  for (let i = 1; i <= Math.max(n, 10); i++) {
+    console.log(i);
+  }
+}
+// Time Complexity: O(n)
+
+function logAtMost10(n) {
+  for (let i = 1; i <= Math.min(n, 10); i++) {
+    console.log(i);
+  }
+}
+// Time Complexity: O(1)
+
+function onlyNumsAtEvenIndexes(nums) {
+  let numsAtEvens = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      numsAtEvens.push(nums[i]);
+    }
+  }
+
+  return numsAtEvens;
+}
+// Time Complexity: O(n)
+
+function runningSubtotals(nums) {
+  let subtotals = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let subtotal = 0;
+    for (let j = 0; j <= i; j++) {
+      subtotal += nums[j];
+    }
+    subtotals.push(subtotal);
+  }
+
+  return subtotals;
+}
+// Time Complexity: O(n)
+// Outer loop scales with nums, inner loop does not.
+
+const VOWELS = "aeiouAEIOU";
+
+function vowelsCounts(word) {
+  let vowelsToCounts = {};
+
+  for (let char of word) {
+    if (VOWELS.includes(char)) {
+      if (char in vowelsToCounts) {
+        vowelsToCounts[char] += 1;
+      } else {
+        vowelsToCounts[char] = 1;
+      }
+    }
+  }
+
+  return vowelsToCounts;
+}
